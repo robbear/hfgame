15b678b4ca1884286669a8aef81bcdfb6859a168
Rob Bearman <robbear@hyperfine.com>
2013-05-30 07:55:49 -0700

    Refactor server-side build into setup_web.cmd for bare-bones upload, and azureserverbuild.cmd. The latter allows changes to the server build without the need to constantly recreate the upload package. The upload package is now behaviorally agnostic and simply serves as an upload payload to bootstrap the server-side build. The actual build instructions are pulled from git cloning the project to the server.

--- NodeAPI/NodeWebSite/bin/setup_web.cmd
+++ NodeAPI/NodeWebSite/bin/setup_web.cmd
@@ -7,50 +7,14 @@ if exist "success.txt" (
 
 cd /d "%~dp0"
 
-if "%EMULATED%"=="true" if DEFINED APPCMD goto emulator_setup
-if "%EMULATED%"== "true" exit /b 0
-
 echo Granting permissions for Network Service to the web root directory...
 icacls ..\ /grant "Network Service":(OI)(CI)W
 if %ERRORLEVEL% neq 0 goto error
 echo OK
 
 echo Downloading tools from http://hfapi.blob.core.windows.net/deployment-files
-curl -O http://hfapi.blob.core.windows.net/deployment-files/7z.exe
-if %ERRORLEVEL% neq 0 goto error
-curl -O http://hfapi.blob.core.windows.net/deployment-files/7z.dll
-if %ERRORLEVEL% neq 0 goto error
 curl -O http://hfapi.blob.core.windows.net/deployment-files/gitinstall.exe
 if %ERRORLEVEL% neq 0 goto error
-curl -O http://hfapi.blob.core.windows.net/deployment-files/iisnode.msi
-if %ERRORLEVEL% neq 0 goto error
-curl -O http://hfapi.blob.core.windows.net/deployment-files/vcredist_x64.exe
-if %ERRORLEVEL% neq 0 goto error
-
-REM - we'll use the following when Joyent fixes the msi installer for LocalSystem installation.
-REM - See issue 4012 (https://github.com/joyent/node/issues/4021)
-REM curl -O http://hfapi.blob.core.windows.net/deployment-files/node-v0.10.8-x64.msi
-REM if %ERRORLEVEL% neq 0 goto error
-REM Instead, we'll just unzip our packaged version
-curl -O http://hfapi.blob.core.windows.net/deployment-files/nodejs.zip
-if %ERRORLEVEL% neq 0 goto error
-echo Unpacking nodejs to the "%programfiles%\nodejs" directory
-7z x -y nodejs.zip -o"%programfiles%"
-if %ERRORLEVEL% neq 0 goto error
-
-echo OK
-
-echo Installing Visual Studio 2010 C++ Redistributable Package...
-vcredist_x64.exe /q
-if %ERRORLEVEL% neq 0 goto error
-echo OK
-
-REM - we'll use the following when Joyent fixes the msi installer for LocalSystem installation.
-REM - See issue 4012 (https://github.com/joyent/node/issues/4021)
-REM echo Installing Node.js
-REM start /wait msiexec.exe /quiet /i node-v0.10.8-x64.msi ADDDEFAULT=NodeRunTime,npm
-REM if %ERRORLEVEL% neq 0 goto error
-REM echo OK
 
 echo Installing Git
 start /wait gitinstall.exe /verysilent /nocancel /suppressmsgboxes
@@ -62,46 +26,15 @@ echo Cloning hfgame
 if %ERRORLEVEL% neq 0 goto error
 echo OK
 
-echo Building version.txt file
-cd hfgame\NodeAPI
-"%ProgramFiles(x86)%\Git\bin\git.exe" rev-parse HEAD > NodeWebSite\version.txt
-set /p versionstring=<NodeWebSite\version.txt
-if %ERRORLEVEL% neq 0 goto error
-cd ..\..
-
-echo Building timestamp.txt file
-echo %date:~-4,4%%date:~-10,2%%date:~-7,2%%time:~0,2%%time:~3,2%%time:~6,2%| "%ProgramFiles(x86)%\Git\bin\sed.exe" 's/ /0/g' > hfgame\NodeAPI\NodeWebSite\timestamp.txt
-set /p timestamp=<hfgame\NodeAPI\NodeWebSite\timestamp.txt
-if %ERRORLEVEL% neq 0 goto error
-
-REM echo Replacing staticfiles path string with versionstring
-REM "%ProgramFiles(x86)%\Git\bin\find.exe" ./hfgame/NodeAPI/NodeWebSite/views -name "*.html" -exec "%ProgramFiles(x86)%\Git\bin\sed.exe" -i "s/staticfiles/%versionstring%/g" '{}' ;
-REM if %ERRORLEVEL% neq 0 goto error
-
-REM echo Renaming staticfiles directory to %versionstring%
-REM ren hfgame\NodeAPI\NodeWebSite\public\staticfiles %versionstring%
-REM if %ERRORLEVEL% neq 0 goto error
-
-echo Copying NodeWebSite to approot
-xcopy hfgame\NodeAPI\NodeWebSite ..\ /c /e /y /EXCLUDE:exclude.txt
-md ..\models
-xcopy hfgame\models ..\models /c /e
-copy /y ..\Web.cloud.config ..\Web.config
-
-echo Running npm install
-cd ..
-echo npm LOG > npmlog.txt
-call "%ProgramFiles%\nodejs\npm.cmd" install 1>> npmlog.txt 2>> npmlog_error.txt
-cd bin
-echo OK
-
-echo Installing iisnode...
-start /wait msiexec.exe /quiet /i iisnode.msi
+echo Copying azureserverbuild.cmd to this directory
+copy /y hfgame\NodeAPI\NodeWebSite\azureserverbuild\azureserverbuild.cmd .\
 if %ERRORLEVEL% neq 0 goto error
 echo OK
 
-echo Removing repository
-rd /s /q hfgame
+echo Running azureserverbuild.cmd
+call azureserverbuild.cmd
+if %ERRORLEVEL% neq 0 goto error
+echo OK
 
 echo Application installation successful > "success.txt"
 echo SUCCESS
@@ -111,28 +44,3 @@ exit /b 0
 
 echo FAILED
 exit /b -1
-
-:emulator_setup
-echo Running in emulator adding iisnode to application host config
-FOR /F "tokens=1,2 delims=/" %%a in ("%APPCMD%") DO set FN=%%a&set OPN=%%b
-if "%OPN%"=="%OPN:apphostconfig:=%" (
-    echo "Could not parse appcmd '%appcmd% for configuration file, exiting"
-    goto error
-)
-
-set IISNODE_BINARY_DIRECTORY=%programfiles%\Microsoft SDKs\Windows Azure\PowerShell\Azure\x86
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" set IISNODE_BINARY_DIRECTORY=%programfiles(x86)%\Microsoft SDKs\Windows Azure\PowerShell\Azure\x64
-
-echo "Using iisnode binaries location '%IISNODE_BINARY_DIRECTORY%'"
-echo installing iisnode module using AppCMD alias %appcmd%
-%appcmd% install module /name:"iisnode" /image:"%IISNODE_BINARY_DIRECTORY%\iisnode.dll"
-
-set apphostconfigfile=%OPN:apphostconfig:=%
-powershell -c "set-executionpolicy unrestricted"
-powershell .\ChangeConfig.ps1 %apphostconfigfile%
-if %ERRORLEVEL% neq 0 goto error
-
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" set
-copy /y "%IISNODE_BINARY_DIRECTORY%\iisnode_schema.xml" "%programfiles%\IIS Express\config\schema\iisnode_schema.xml"
-if %ERRORLEVEL% neq 0 goto error
-exit /b 0
